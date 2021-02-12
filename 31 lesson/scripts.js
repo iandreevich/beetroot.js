da64c9c9389bb2896e6e1ba860aeb7472dfe7cdb
iandreevich <iadnreevich228@gmail.com>
2021-02-12 23:08:48 +0200

    added 31 homework

---
+++ 31 lesson/scripts.js
@@ -0,0 +1,9 @@
+$(".slider").slick({
+  speed: 300,
+  slidesToShow: 3.5,
+  slidesToScroll: 1,
+  infinite: false,
+  arrows: true,
+  prevArrow: $(".slider__prev"),
+  nextArrow: $(".slider__next"),
+});
